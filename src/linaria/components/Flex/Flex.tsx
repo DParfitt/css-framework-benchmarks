9bff1fe76716d61ae54493b65d1338889c6b7c38
Dillon Parfitt <dparfitt@xebia.com>
2023-05-11 18:52:03 +0200

    fix flex

--- src/linaria/components/Flex/Flex.tsx
+++ src/linaria/components/Flex/Flex.tsx
@@ -1,10 +1,12 @@
 import { ForwardRefRenderFunction, ReactNode } from "react";
 import { Box } from "../Box/Box";
-import type { Atoms } from "../../css/sprinkles.css";
 
-export const Flex: ForwardRefRenderFunction<HTMLDivElement, { children?: ReactNode; className?: string } & Atoms> = ({ children, ...props }, ref) => {
+export const Flex: ForwardRefRenderFunction<
+  HTMLDivElement,
+  { children?: ReactNode; className?: string }
+> = ({ children, ...props }, ref) => {
   return (
-    <Box display="flex" ref={ref} {...props}>
+    <Box ref={ref} {...props}>
       {children}
     </Box>
   );
