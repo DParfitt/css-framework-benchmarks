25ca7bfa310710b4c343110cf57447404e6a4004
Dillon Parfitt <dparfitt@xebia.com>
2023-05-11 09:16:38 +0200

    added a bigger page

--- src/app/vanilla-extract/page.tsx
+++ src/app/vanilla-extract/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "../../vanilla-extract/components/Button/Button";
 import { CTASection } from "../../vanilla-extract/components/CTASection/CTASection";
+import { Heading } from "../../vanilla-extract/components/Heading/Heading";
 
 export default function Home() {
   return (
@@ -39,6 +41,226 @@ export default function Home() {
           alt: "random picture",
         }}
       />
+      <Heading as="h1" size="h1" variant="primary">
+        I am a h1 primary heading with a size of h1
+      </Heading>
+      <Heading as="h2" size="h1" variant="primary">
+        I am a h2 primary heading with a size of h1
+      </Heading>
+      <Heading as="h3" size="h1" variant="primary">
+        I am a h3 primary heading with a size of h1
+      </Heading>
+      <Heading as="h4" size="h1" variant="primary">
+        I am a h4 primary heading with a size of h1
+      </Heading>
+      <Heading as="h5" size="h1" variant="primary">
+        I am a h5 primary heading with a size of h1
+      </Heading>
+      <Heading as="h6" size="h1" variant="primary">
+        I am a h6 primary heading with a size of h1
+      </Heading>
+      <Heading as="h1" size="h2" variant="primary">
+        I am a h1 primary heading with a size of h2
+      </Heading>
+      <Heading as="h2" size="h2" variant="primary">
+        I am a h2 primary heading with a size of h2
+      </Heading>
+      <Heading as="h3" size="h2" variant="primary">
+        I am a h3 primary heading with a size of h2
+      </Heading>
+      <Heading as="h4" size="h2" variant="primary">
+        I am a h4 primary heading with a size of h2
+      </Heading>
+      <Heading as="h5" size="h2" variant="primary">
+        I am a h5 primary heading with a size of h2
+      </Heading>
+      <Heading as="h6" size="h2" variant="primary">
+        I am a h6 primary heading with a size of h2
+      </Heading>
+      <Heading as="h1" size="h3" variant="primary">
+        I am a h1 primary heading with a size of h3
+      </Heading>
+      <Heading as="h2" size="h3" variant="primary">
+        I am a h2 primary heading with a size of h3
+      </Heading>
+      <Heading as="h3" size="h3" variant="primary">
+        I am a h3 primary heading with a size of h3
+      </Heading>
+      <Heading as="h4" size="h3" variant="primary">
+        I am a h4 primary heading with a size of h3
+      </Heading>
+      <Heading as="h5" size="h3" variant="primary">
+        I am a h5 primary heading with a size of h3
+      </Heading>
+      <Heading as="h6" size="h3" variant="primary">
+        I am a h6 primary heading with a size of h3
+      </Heading>
+      <Heading as="h1" size="h4" variant="primary">
+        I am a h1 primary heading with a size of h4
+      </Heading>
+      <Heading as="h2" size="h4" variant="primary">
+        I am a h2 primary heading with a size of h4
+      </Heading>
+      <Heading as="h3" size="h4" variant="primary">
+        I am a h3 primary heading with a size of h4
+      </Heading>
+      <Heading as="h4" size="h4" variant="primary">
+        I am a h4 primary heading with a size of h4
+      </Heading>
+      <Heading as="h5" size="h4" variant="primary">
+        I am a h5 primary heading with a size of h4
+      </Heading>
+      <Heading as="h6" size="h4" variant="primary">
+        I am a h6 primary heading with a size of h4
+      </Heading>
+      <Heading as="h1" size="h5" variant="primary">
+        I am a h1 primary heading with a size of h5
+      </Heading>
+      <Heading as="h2" size="h5" variant="primary">
+        I am a h2 primary heading with a size of h5
+      </Heading>
+      <Heading as="h3" size="h5" variant="primary">
+        I am a h3 primary heading with a size of h5
+      </Heading>
+      <Heading as="h4" size="h5" variant="primary">
+        I am a h4 primary heading with a size of h5
+      </Heading>
+      <Heading as="h5" size="h5" variant="primary">
+        I am a h5 primary heading with a size of h5
+      </Heading>
+      <Heading as="h6" size="h5" variant="primary">
+        I am a h6 primary heading with a size of h5
+      </Heading>
+      <Heading as="h1" size="h6" variant="primary">
+        I am a h1 primary heading with a size of h6
+      </Heading>
+      <Heading as="h2" size="h6" variant="primary">
+        I am a h2 primary heading with a size of h6
+      </Heading>
+      <Heading as="h3" size="h6" variant="primary">
+        I am a h3 primary heading with a size of h6
+      </Heading>
+      <Heading as="h4" size="h6" variant="primary">
+        I am a h4 primary heading with a size of h6
+      </Heading>
+      <Heading as="h5" size="h6" variant="primary">
+        I am a h5 primary heading with a size of h6
+      </Heading>
+      <Heading as="h6" size="h6" variant="primary">
+        I am a h6 primary heading with a size of h6
+      </Heading>
+
+      <Heading as="h1" size="h1" variant="secondary">
+        I am a h1 secondary heading with a size of h1
+      </Heading>
+      <Heading as="h2" size="h1" variant="secondary">
+        I am a h2 secondary heading with a size of h1
+      </Heading>
+      <Heading as="h3" size="h1" variant="secondary">
+        I am a h3 secondary heading with a size of h1
+      </Heading>
+      <Heading as="h4" size="h1" variant="secondary">
+        I am a h4 secondary heading with a size of h1
+      </Heading>
+      <Heading as="h5" size="h1" variant="secondary">
+        I am a h5 secondary heading with a size of h1
+      </Heading>
+      <Heading as="h6" size="h1" variant="secondary">
+        I am a h6 secondary heading with a size of h1
+      </Heading>
+      <Heading as="h1" size="h2" variant="secondary">
+        I am a h1 secondary heading with a size of h2
+      </Heading>
+      <Heading as="h2" size="h2" variant="secondary">
+        I am a h2 secondary heading with a size of h2
+      </Heading>
+      <Heading as="h3" size="h2" variant="secondary">
+        I am a h3 secondary heading with a size of h2
+      </Heading>
+      <Heading as="h4" size="h2" variant="secondary">
+        I am a h4 secondary heading with a size of h2
+      </Heading>
+      <Heading as="h5" size="h2" variant="secondary">
+        I am a h5 secondary heading with a size of h2
+      </Heading>
+      <Heading as="h6" size="h2" variant="secondary">
+        I am a h6 secondary heading with a size of h2
+      </Heading>
+      <Heading as="h1" size="h3" variant="secondary">
+        I am a h1 secondary heading with a size of h3
+      </Heading>
+      <Heading as="h2" size="h3" variant="secondary">
+        I am a h2 secondary heading with a size of h3
+      </Heading>
+      <Heading as="h3" size="h3" variant="secondary">
+        I am a h3 secondary heading with a size of h3
+      </Heading>
+      <Heading as="h4" size="h3" variant="secondary">
+        I am a h4 secondary heading with a size of h3
+      </Heading>
+      <Heading as="h5" size="h3" variant="secondary">
+        I am a h5 secondary heading with a size of h3
+      </Heading>
+      <Heading as="h6" size="h3" variant="secondary">
+        I am a h6 secondary heading with a size of h3
+      </Heading>
+      <Heading as="h1" size="h4" variant="secondary">
+        I am a h1 secondary heading with a size of h4
+      </Heading>
+      <Heading as="h2" size="h4" variant="secondary">
+        I am a h2 secondary heading with a size of h4
+      </Heading>
+      <Heading as="h3" size="h4" variant="secondary">
+        I am a h3 secondary heading with a size of h4
+      </Heading>
+      <Heading as="h4" size="h4" variant="secondary">
+        I am a h4 secondary heading with a size of h4
+      </Heading>
+      <Heading as="h5" size="h4" variant="secondary">
+        I am a h5 secondary heading with a size of h4
+      </Heading>
+      <Heading as="h6" size="h4" variant="secondary">
+        I am a h6 secondary heading with a size of h4
+      </Heading>
+      <Heading as="h1" size="h5" variant="secondary">
+        I am a h1 secondary heading with a size of h5
+      </Heading>
+      <Heading as="h2" size="h5" variant="secondary">
+        I am a h2 secondary heading with a size of h5
+      </Heading>
+      <Heading as="h3" size="h5" variant="secondary">
+        I am a h3 secondary heading with a size of h5
+      </Heading>
+      <Heading as="h4" size="h5" variant="secondary">
+        I am a h4 secondary heading with a size of h5
+      </Heading>
+      <Heading as="h5" size="h5" variant="secondary">
+        I am a h5 secondary heading with a size of h5
+      </Heading>
+      <Heading as="h6" size="h5" variant="secondary">
+        I am a h6 secondary heading with a size of h5
+      </Heading>
+      <Heading as="h1" size="h6" variant="secondary">
+        I am a h1 secondary heading with a size of h6
+      </Heading>
+      <Heading as="h2" size="h6" variant="secondary">
+        I am a h2 secondary heading with a size of h6
+      </Heading>
+      <Heading as="h3" size="h6" variant="secondary">
+        I am a h3 secondary heading with a size of h6
+      </Heading>
+      <Heading as="h4" size="h6" variant="secondary">
+        I am a h4 secondary heading with a size of h6
+      </Heading>
+      <Heading as="h5" size="h6" variant="secondary">
+        I am a h5 secondary heading with a size of h6
+      </Heading>
+      <Heading as="h6" size="h6" variant="secondary">
+        I am a h6 secondary heading with a size of h6
+      </Heading>
+
+      <Button variant="solid">I am a solid variant</Button>
+      <Button variant="outline">I am an outline variant</Button>
     </>
   );
 }
